38a04a08c2302d6ea57c83996d9ddccee29dd730
Carles Bruguera <carlesba@gmail.com>
2014-12-01 09:16:48 +0100

    Preparing release 4.0.8

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = [
 ]
 
 setup(name='maxscripts',
-      version='4.0.8.dev0',
+      version='4.0.8',
       description='maxscripts',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
