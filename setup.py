7169821b55c686fd7e738f3f9ad3eb98b77d7129
Carles Bruguera <carlesba@gmail.com>
2014-12-01 10:24:19 +0100

    Back to development: 4.0.11

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requires = [
 ]
 
 setup(name='maxscripts',
-      version='4.0.10',
+      version='4.0.11.dev0',
       description='maxscripts',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
