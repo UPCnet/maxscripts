04e5b6aaf1ed7d985e12e3c846802ae7bf9d0e2a
Carles Bruguera <carlesba@gmail.com>
2015-01-29 13:51:24 +0100

    Use maxutils.mongodb to acces mongo

--- maxscripts/mongoindexes.py
+++ maxscripts/mongoindexes.py
@@ -3,11 +3,11 @@ import argparse
 import ConfigParser
 
 import sys
-import pymongo
+from maxutils import mongodb
 import re
 
 
-def as_bool(value):
+def asbool(value):
     return not str(value).lower() in ['0', 'false', 'none', '']
 
 
@@ -20,30 +20,45 @@ class CreateMongoIndexes(object):  # pragma: no cover
         self.maxconfig = ConfigParser.ConfigParser()
         self.maxconfig.read(self.options.maxconfigfile)
 
-        try:
-            self.cluster = as_bool(self.maxconfig.get('app:main', 'mongodb.cluster').capitalize())
-            self.standaloneserver = self.maxconfig.get('app:main', 'mongodb.url')
-            self.hosts = self.maxconfig.get('app:main', 'mongodb.hosts')
-            self.replica_set = self.maxconfig.get('app:main', 'mongodb.replica_set')
-            self.db_name = self.maxconfig.get('app:main', 'mongodb.db_name')
+        self.common = ConfigParser.ConfigParser()
+        self.common.read(self.options.commonfile)
 
-        except:
-            print('You must provide a valid configuration .ini file.')
-            sys.exit()
+        self.db_name = self.maxconfig.get('app:main', 'mongodb.db_name')
+
+        self.cluster = asbool(self.common.get('mongodb', 'cluster'))
+        self.standaloneserver = self.common.get('mongodb', 'url')
+        self.clustermembers = self.common.get('mongodb', 'hosts')
+        self.replicaset = self.common.get('mongodb', 'replica_set')
+
+        self.mongo_auth = asbool(self.common.get('mongodb', 'auth'))
+        self.mongo_authdb = self.common.get('mongodb', 'authdb')
+        self.mongo_username = self.common.get('mongodb', 'username')
+        self.mongo_password = self.common.get('mongodb', 'password')
 
     def run(self):
 
-        # Check if we're connecting to a cluster
+        # Mongodb connection initialization
+        cluster_enabled = self.cluster
+        auth_enabled = self.mongo_auth
+        mongodb_uri = self.clustermembers if cluster_enabled else self.standaloneserver
+
+        conn = mongodb.get_connection(
+            mongodb_uri,
+            use_greenlets=True,
+            cluster=self.replica_set if cluster_enabled else None)
+
+        # Log the kinf of connection we're making
         if self.cluster:
             print 'Connecting to database @ cluster "{}" ...'.format(self.replica_set)
-            self.connection = pymongo.MongoReplicaSetClient(self.hosts, replicaSet=self.replica_set)
-
-        #Otherwise make a single connection
         else:
             print 'Connecting to database @ {} ...'.format(self.standaloneserver)
-            self.connection = pymongo.Connection(self.standaloneserver)
 
-        self.db = self.connection[self.db_name]
+        db = mongodb.get_database(
+            conn,
+            self.db_name,
+            username=self.mongo_username if auth_enabled else None,
+            password=self.mongo_password if auth_enabled else None,
+            authdb=self.mongo_authdb if auth_enabled else None)
 
         indexes_file = open(self.options.indexesfile).read()
         clean = re.sub(r'(?:^|\n)#+[^\n]*', r'', indexes_file)
@@ -52,7 +67,7 @@ class CreateMongoIndexes(object):  # pragma: no cover
         for collection, index_key in indexes:
             sys.stdout.write('. ')
             sys.stdout.flush()
-            self.db[collection].create_index(index_key)
+            db[collection].create_index(index_key)
             count += 1
         if count:
             print "\nAdded {} indexes to database '{}'".format(count, self.db_name)
@@ -75,6 +90,14 @@ def main(argv=sys.argv, quiet=False):  # pragma: no cover
         type=str,
         default='config/max.ini',
         help=("Max configuration file"))
+
+    parser.add_argument(
+        '-o', '--common',
+        dest='commonfile',
+        type=str,
+        default='config/common.ini',
+        help=("Common configuration file"))
+
     options = parser.parse_args()
 
     command = CreateMongoIndexes(options)
